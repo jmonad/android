287d9c66ed6bddaed21c6a58326d678cf922e2da
Lucas Teske <lucas@teske.net.br>
2016-04-16 16:01:56 -0300

    Added simple performance test and fixed problems
    *	Using AndroidAnnotations now for the view on Sample App
    *	Added Test0 that tests the map as described in Seq page

--- app/src/main/java/com/monadroid/sampleapp/HomeActivity.java
+++ app/src/main/java/com/monadroid/sampleapp/HomeActivity.java
@@ -1,13 +1,64 @@
 package com.monadroid.sampleapp;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.TextView;
 
+import com.jmonad.seq.Seq;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Background;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.UiThread;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_home)
 public class HomeActivity extends AppCompatActivity {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_home);
+  private static final int test0N = 1000000;
+
+  @ViewById
+  TextView contentText;
+
+  @UiThread
+  protected void printText(String text) {
+    contentText.append(text + "\n");
+  }
+
+  @AfterViews
+  protected void afterViews() {
+    test0();
+  }
+
+  @Background
+  protected void test0() {
+    printText("Starting test0");
+    Integer[] integers = new Integer[test0N];
+
+    printText("   - Filling " + test0N + " integers to test.");
+    for (int i=0; i < test0N; i++) {
+      integers[i] = i;
+    }
+
+    long startTime = System.currentTimeMillis();
+    long totalTime = 0;
+    long endTime;
+
+    // Initialization Test
+    printText("   - Starting test 0");
+    Seq<Integer> values = new Seq<>(integers);
+    endTime = System.currentTimeMillis();
+    totalTime += (endTime - startTime);
+    printText("   - Created Seq in " + (endTime - startTime) + " ms");
+
+    // Map Test
+    printText("   - Mapping x -> x * 2");
+    startTime = System.currentTimeMillis();
+    values = values.map(x -> x * 2);
+    endTime = System.currentTimeMillis();
+    totalTime += (endTime - startTime);
+    printText("   - Mapped x -> x * 2 in " + (endTime - startTime) + " ms");
+
+    printText("   - Total operating time: " + totalTime + " ms for test0");
   }
 
 }
